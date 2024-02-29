92b90bbc16ad53c4e6bf0b3819769370de47a364
MikhailIzegov <mikhail.izegov@yandex.ru>
2024-02-29 04:07:03 +0300

    fixed conftest.py for Jenkins

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,30 @@
-from selene import browser
 import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selene import Browser, Config
 
 
 @pytest.fixture(scope="function", autouse=True)
-def open_browser():
+def setup_browser(request):
+    browser_version = '100.0'
+    options = Options()
+    selenoid_capabilities = {
+        'browserName': 'chrome',
+        'browserVersion': browser_version,
+        'selenoid:options': {
+            'enableVNC': True,
+            'enableVideo': True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f'https://user1:1234@selenoid.autotests.cloud/wd/hub',
+        options=options
+    )
+
+    browser = Browser(Config(driver))
     browser.config.base_url = 'https://demoqa.com'
 
-    yield
+    yield browser
 
     browser.quit()
