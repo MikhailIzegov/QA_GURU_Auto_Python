06763d09146a253c0a4aaa1f7b6858c699929d9b
MikhailIzegov <mikhail.izegov@yandex.ru>
2024-02-29 20:07:27 +0300

    another fix for conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,11 +7,10 @@ import os
 
 @pytest.fixture(scope="function", autouse=True)
 def setup_browser(request):
-    browser_version = '100.0'
     options = Options()
     selenoid_capabilities = {
         'browserName': 'chrome',
-        'browserVersion': browser_version,
+        'browserVersion': '100.0',
         'selenoid:options': {
             'enableVNC': True,
             'enableVideo': True
@@ -19,10 +18,12 @@ def setup_browser(request):
     }
     options.capabilities.update(selenoid_capabilities)
 
-    browser.config.driver_options = options
-    browser.config.driver_remote_url = (
-        f'https://user1:1234@selenoid.autotests.cloud/wd/hub'
+    driver = webdriver.Remote(
+        command_executor=f'https://user1:1234@selenoid.autotests.cloud/wd/hub',
+        options=options
     )
+
+    browser.config.driver = driver
 
     yield browser
 
