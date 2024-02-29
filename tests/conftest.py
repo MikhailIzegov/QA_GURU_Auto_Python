874e7145d74ad922714b2245821dc7e94c91dd71
MikhailIzegov <mikhail.izegov@yandex.ru>
2024-03-01 00:44:46 +0300

    try to comment conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,23 +7,23 @@ from selene import browser
 @pytest.fixture(scope="function", autouse=True)
 def setup_browser():
     browser.config.base_url = 'https://demoqa.com'
-    options = Options()
-    selenoid_capabilities = {
-        'browserName': 'chrome',
-        'browserVersion': '100.0',
-        'selenoid:options': {
-            'enableVNC': True,
-            'enableVideo': True
-        }
-    }
-    options.capabilities.update(selenoid_capabilities)
-
-    driver = webdriver.Remote(
-        command_executor=f'https://user1:1234@selenoid.autotests.cloud/wd/hub',
-        options=options
-    )
-
-    browser.config.driver = driver
+    # options = Options()
+    # selenoid_capabilities = {
+    #     'browserName': 'chrome',
+    #     'browserVersion': '100.0',
+    #     'selenoid:options': {
+    #         'enableVNC': True,
+    #         'enableVideo': True
+    #     }
+    # }
+    # options.capabilities.update(selenoid_capabilities)
+    #
+    # driver = webdriver.Remote(
+    #     command_executor=f'https://user1:1234@selenoid.autotests.cloud/wd/hub',
+    #     options=options
+    # )
+    #
+    # browser.config.driver = driver
 
     yield
 
