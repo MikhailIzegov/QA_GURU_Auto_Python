7e05b49e1fd7fcb532732f66fe19608bae68a0d6
MikhailIzegov <mikhail.izegov@yandex.ru>
2024-03-02 20:08:36 +0300

    added png and logs to allure

--- tests/conftest.py
+++ tests/conftest.py
@@ -31,3 +31,9 @@ def setup_browser():
     html = browser.driver.page_source
     allure.attach(body=html, name='page_source', attachment_type=allure.attachment_type.HTML, extension='.html')
     browser.quit()
+
+    png = browser.driver.get_screenshot_as_png()
+    allure.attach(body=png, name='screenshot', attachment_type=allure.attachment_type.PNG, extension='.png')
+
+    log = ''.join(f'{text}\n' for text in browser.driver.get_log(log_type='browser'))
+    allure.attach(log, 'browser_logs', allure.attachment_type.TEXT, '.log')
