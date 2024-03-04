17a09b19995d063f694c7792f8230ab01a44179c
MikhailIzegov <mikhail.izegov@yandex.ru>
2024-03-04 19:00:04 +0300

    test-parametrize is done

--- pytest_tests/test_parametrize.py
+++ pytest_tests/test_parametrize.py
@@ -2,16 +2,31 @@
 Переопределите параметр с помощью indirect параметризации на уровне теста
 """
 import pytest
+from selene import browser, be, command
 
 
-@pytest.fixture()
-def browser():
-    pass
+@pytest.fixture(params=['desktop', 'mobile'])
+def adjust_size(request):
+    browser.config.base_url = 'https://github.com/'
+    if request.param == 'desktop':
+        browser.driver.maximize_window()
+    elif request.param == 'mobile':
+        browser.config.window_width = 800
+        browser.config.window_height = 600
+
+    yield
+    browser.quit()
 
 
-def test_github_desktop(browser):
-    pass
+@pytest.mark.parametrize('adjust_size', ['desktop'], indirect=True)
+def test_github_desktop(adjust_size):
+    browser.open('/')
+    browser.element('.HeaderMenu-link--sign-in').click()
+    browser.element('.auth-form-body').should(be.visible)
 
 
-def test_github_mobile(browser):
-    pass
+@pytest.mark.parametrize('adjust_size', ['mobile'], indirect=True)
+def test_github_mobile(adjust_size):
+    browser.open('/')
+    browser.element('[aria-label="Toggle navigation"][type="button"').perform(command.js.click)
+    browser.element('.header-menu-wrapper').should(be.visible)
