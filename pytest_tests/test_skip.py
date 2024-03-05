f222d78b0da3e7e53536bfa08ca6d23e8505644d
MikhailIzegov <mikhail.izegov@yandex.ru>
2024-03-05 23:23:16 +0300

    test-skip draft is added

--- pytest_tests/test_skip.py
+++ pytest_tests/test_skip.py
@@ -1,18 +1,43 @@
-"""
-Параметризуйте фикстуру несколькими вариантами размеров окна
-Пропустите мобильный тест, если соотношение сторон десктопное (и наоборот)
-"""
 import pytest
+from selene import browser, be, command
+
+# Определение значений ширины и высоты для различных устройств
+window_sizes = [
+    (1920, 1080),  # Десктоп
+    (375, 812),     # Мобильный
+]
 
 
-@pytest.fixture()
-def browser():
-    pass
+@pytest.fixture(params=window_sizes)
+def adjust_size(request):
+    width, height = request.param
+    browser.config.base_url = 'https://github.com/'
+    browser.driver.set_window_size(width, height)
+
+    yield
+
+    browser.quit()
 
 
-def test_github_desktop(browser):
-    pass
+def test_github_desktop(adjust_size):
+    width, height = browser.driver.get_window_size()['width'], browser.driver.get_window_size()['height']
+
+    # Проверка на соотношение сторон
+    if height > width:
+        pytest.skip("Мобильное соотношение сторон")
+
+    browser.open('/')
+    browser.element('.HeaderMenu-link--sign-in').click()
+    browser.element('.auth-form-body').should(be.visible)
 
 
-def test_github_mobile(browser):
-    pass
+def test_github_mobile(adjust_size):
+    width, height = browser.driver.get_window_size()['width'], browser.driver.get_window_size()['height']
+
+    # Проверка на соотношение сторон
+    if width > height:
+        pytest.skip("Десктопное соотношение сторон")
+
+    browser.open('/')
+    browser.element('[aria-label="Toggle navigation"][type="button"').perform(command.js.click)
+    browser.element('.header-menu-wrapper').should(be.visible)
