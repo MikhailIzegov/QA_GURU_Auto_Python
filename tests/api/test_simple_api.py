066ebd10f0620d8a6185aa7a77486663d804590f
MikhailIzegov <mikhail.izegov@yandex.ru>
2024-03-07 15:25:12 +0300

    more simple api tests are added

--- tests/api/test_simple_api.py
+++ tests/api/test_simple_api.py
@@ -24,7 +24,7 @@ def test_headers():
     assert response.headers['Connection'] == 'keep-alive'
 
 
-def test_create_user():
+def test_post_user():
     response = requests.post(url='https://reqres.in/api/users', json={
             "name": "morpheus",
             "job": "leader"
@@ -37,3 +37,32 @@ def test_create_user():
     schema = load_schema('post_users.json')
     assert response.status_code == 201
     jsonschema.validate(response.json(), schema)
+
+
+def test_put_user():
+    response = requests.put(url='https://reqres.in/api/users/3', json={
+            "name": "morpheus555",
+            "job": "zion resident"
+    })
+
+    schema = load_schema('put_users.json')
+    assert response.status_code == 200
+    jsonschema.validate(response.json(), schema)
+
+
+def test_delete_user():
+    user_to_delete = 3
+    response = requests.delete(url=f'https://reqres.in/api/users/{user_to_delete}')
+
+    assert response.status_code == 204
+
+
+def test_patch_user():
+    response = requests.patch(url='https://reqres.in/api/users/3', json={
+            "name": "morpheus666"
+    })
+
+    schema = load_schema('patch_users.json')
+    assert response.status_code == 200
+    jsonschema.validate(response.json(), schema)
+
